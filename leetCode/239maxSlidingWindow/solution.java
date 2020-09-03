d1e66da43495ead2d75ebe2baca6ff0809742576
longshenzhu <1223006618@qq.com>
2020-09-03 18:02:38 +0800

    ssave

--- leetCode/239maxSlidingWindow/solution.java
+++ leetCode/239maxSlidingWindow/solution.java
@@ -1,7 +1,18 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
+        if(nums.length == 0 || k == 1){
+            return nums;
+        }
         int[] ans = new int[nums.length-k+1];
-        
+        Deque<Integer> deque = new ArrayDeque<>();
+        for(int i=0; i< nums.length; i++){
+            // if(!deque.isEmpty() && deque.peekFirst()){
+
+            // }
+        }
         return ans;
     }
 }
