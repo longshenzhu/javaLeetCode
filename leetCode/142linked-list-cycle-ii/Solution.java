1fd7aadfcf2809f8a3bbe0b9870a65a22ea46dfc
1223006618@qq.com <1223006618@qq.com>
2020-10-10 14:29:44 +0800

    add 142

---
+++ leetCode/142linked-list-cycle-ii/Solution.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        
+        return null;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { 
+            this.val = x;
+            next = null;
+        }
+    }
+    
+}
