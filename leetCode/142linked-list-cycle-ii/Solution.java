15aa88f5c298f32bb89c1670747f6df9fc36a62f
1223006618@qq.com <1223006618@qq.com>
2020-10-10 15:05:35 +0800

    save

--- leetCode/142linked-list-cycle-ii/Solution.java
+++ leetCode/142linked-list-cycle-ii/Solution.java
@@ -1,3 +1,6 @@
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Definition for singly-linked list.
  * class ListNode {
@@ -10,10 +13,44 @@
  * }
  */
 public class Solution {
+
+    //快慢指针法
     public ListNode detectCycle(ListNode head) {
-        
+
+        //fast = low + nb;
+        //fast = 2*low;
+        //重逢时 low走了nb，即n个环，fast走了2n个环
+        ListNode fast = head,low = head;
+        while(fast != null){
+            if(fast.next == null)return null;
+            low = low.next;
+            fast = fast.next.next;
+
+            //找到快慢指针重逢位置
+            if(fast == low){
+                ListNode ptr = head;
+                while(low != ptr){
+                    low = low.next;
+                    ptr = ptr.next;
+                }
+                return ptr;
+            }
+        }
         return null;
     }
+
+    // 哈希表记录法
+    // public ListNode detectCycle(ListNode head) {
+    //     Set<ListNode> visited = new HashSet<ListNode>();
+    //     ListNode cur = head;
+    //     while(cur != null){
+    //         if(visited.contains(cur))return cur;
+    //         visited.add(cur);
+
+    //         cur = cur.next;
+    //     }
+    //     return null;
+    // }
 
     public class ListNode {
         int val;
