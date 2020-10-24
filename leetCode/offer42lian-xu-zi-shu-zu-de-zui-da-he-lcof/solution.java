05522a934c43fced20dad34e6a32a95dba9dfd3f
longshenzhu <1223006618@qq.com>
2020-10-24 23:36:00 +0800

    save

--- leetCode/offer42lian-xu-zi-shu-zu-de-zui-da-he-lcof/solution.java
+++ leetCode/offer42lian-xu-zi-shu-zu-de-zui-da-he-lcof/solution.java
@@ -1,5 +1,15 @@
 class Solution {
     public int maxSubArray(int[] nums) {
+        if(nums == null || nums.length== 0)return 0;
+        int[] dp = new int[nums.length];
         
+        dp[0] = nums[0];
+        int max = dp[0];
+        for (int i = 1; i < nums.length; i++) {
+            dp[i]= dp[i-1]>=0? (dp[i-1] + nums[i]): nums[i];
+            max = Math.max(max, dp[i]);
+        }
+
+        return max;
     }
 }
