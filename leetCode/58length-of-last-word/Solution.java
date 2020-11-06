566497b0d9b06c0ec2be02275915fb6f53644e35
1223006618@qq.com <1223006618@qq.com>
2020-11-06 18:59:13 +0800

    save

---
+++ leetCode/58length-of-last-word/Solution.java
@@ -0,0 +1,47 @@
+class Solution {
+    int lengthOfLastWord(String s){
+        int len = s.length(), lastWordLen = 0;
+        if (len == 0) return 0;
+    
+        for (int i = len - 1; i >= 0; i--) {
+            if (s.charAt(i) != ' ') lastWordLen++;
+            if (s.charAt(i) == ' ' && lastWordLen > 0) break;
+        }
+    
+        return lastWordLen;
+    }
+    // public int lengthOfLastWord(String s) {
+    //     if(s == null || s.length()==0) {
+    //         return 0;
+    //     }
+    //     String newStr = s.trim();
+    //     int len = newStr.length();
+    //     for (int i = len-1; i >=0; i--) {
+    //         Character item = newStr.charAt(i);
+    //         if(Character.isWhitespace(item)){
+    //             return len - i - 1;
+    //         }
+    //     }
+    //     return len;
+    // }
+    // public int lengthOfLastWord(String s) {
+    //     if(s == null || s.length()==0) {
+    //         return 0;
+    //     }
+    //     int len = 0;
+    //     boolean lastFlag = false;
+    //     for (int i = s.length()-1; i >=0; i--) {
+    //         Character item = s.charAt(i);
+    //         if(Character.isWhitespace(item)){
+    //             if(!lastFlag){
+    //                 continue;
+    //             }
+    //             return len;
+    //         }else{
+    //             lastFlag = true;
+    //             len++;
+    //         }
+    //     }
+    //     return len;
+    // }
+}
