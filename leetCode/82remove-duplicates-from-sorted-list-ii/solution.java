db78c94512665826d19e128c97d88b21314ce559
1223006618@qq.com <1223006618@qq.com>
2020-11-11 17:20:45 +0800

    save

--- leetCode/82remove-duplicates-from-sorted-list-ii/solution.java
+++ leetCode/82remove-duplicates-from-sorted-list-ii/solution.java
@@ -28,6 +28,7 @@ class Solution {
         return node0.next;
     }
 
+    //方法二
     // public ListNode deleteDuplicates(ListNode head) {
     //     if(head==null || head.next==null) {
     //         return head;
