772f84c442c236088327a3057606895b284eeea4
1223006618@qq.com <1223006618@qq.com>
2020-11-25 18:54:10 +0800

    add 64

--- leetCode/64minimum-path-sum/Solution.java
+++ leetCode/64minimum-path-sum/Solution.java
@@ -1,5 +1,39 @@
 class Solution {
     public int minPathSum(int[][] grid) {
-        return 0;
+        if(grid == null || grid.length == 0) return 0;
+        int row = grid.length;
+        int column = grid[0].length;
+
+        //1、定义状态
+        int[][] dp = new int[row][column];
+        dp[0][0] = grid[0][0];
+        //2、dp方程
+        for (int i = 1; i < row; i++) {
+            dp[i][0] = dp[i - 1][0] + grid[i][0];
+        }
+        for (int j = 1; j < column; j++) {
+            dp[0][j] = dp[0][j - 1] + grid[0][j];
+        }
+        for (int i = 1; i < row; i++) {
+            for (int j = 1; j < column; j++) {
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+            }
+        }
+        //3、起始值
+        return dp[row - 1][column - 1];
+        // for (int i = 0; i < raw; i++) {
+        //     for (int j = 0; j < column; j++) {
+        //         if(i == 0 && j == 0){
+        //             dp[i][j] = grid[0][0];
+        //         }else if(i==0){
+        //             dp[i][j] = dp[0][j-1] + grid[0][j];
+        //         }else if(j==0){
+        //             dp[i][j] = dp[i-1][0] + grid[i][0];
+        //         }else{
+        //             dp[i][j] = Math.min(dp[i-1][j] + grid[i][j], dp[i][j-1] + grid[i][j]);
+        //         }
+        //     }
+        // }
+        // return dp[raw - 1][column - 1];
     }
 }
--- leetCode/排序算法/3插入/Solution.java
+++ leetCode/排序算法/3插入/Solution.java
@@ -5,12 +5,15 @@ class Solution {
         int[] array = Arrays.copyOf(sourceArray, sourceArray.length);
         if (array == null || array.length <=1)return array;
         for (int i = 1; i < array.length; i++) {
-            for (int j = i; j > 0; j--) {
-                if(array[j-1] <array[j]) {
-                    int temp = array[j];
-                    array[j] = array[j-1];
-                    array[j-1] = temp;
-                }
+            int temp = array[i];
+            int j = i;
+            while (j > 0 && array[j-1] > temp) {
+                array[j] = array[j-1];
+                j --;
+            }
+            // 插入
+            if (j != i) {
+                array[j] = temp;
             }
         }
         return array;
